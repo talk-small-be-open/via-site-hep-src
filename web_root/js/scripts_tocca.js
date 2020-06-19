626bc7f90313e8c3e05fbcd4d934624808e1c0c5
Andreas Brodbeck <andreas.brodbeck@mindclue.ch>
2020-06-19 18:54:34 +0200

    progress

--- web_root/js/scripts_tocca.js
+++ web_root/js/scripts_tocca.js
@@ -1,1 +1,50 @@
-// JavaScript für VIA Website1
+// JavaScript für VIA Tocca
+
+
+function onMapPairsDrop(dropzone, draggable) {
+  $(dropzone).children('div.rightObject').prepend(draggable);
+	$(draggable).css({top:0, left:0});
+}
+
+function onDragDropDrop(dropzone, draggable) {
+  $(dropzone).append(draggable);
+	$(draggable).css({top:0, left:0});
+}
+
+
+function textinput_markAsEmpty(event, elementId) {
+	var element = $('#'+elementId);
+	event.stopPropagation();
+
+	element.toggleClass('markedAsEmpty');
+
+	if (element.hasClass('markedAsEmpty')) {
+		$(event.target).addClass('active');
+		element.val('---')
+	} else {
+		$(event.target).removeClass('active');
+		element.val('')		
+	}	
+}
+
+
+/* Haupt JS init */
+$(document).ready(function(){
+
+	JoelPurra.PlusAsTab.setOptions({
+		// Use the enter key as tab keys
+		key: [13]
+	});	
+
+	$('.popover').webuiPopover({trigger:'hover'});
+	$('span.dictionaryEntry').webuiPopover({trigger:'hover'});
+	$('input.clozeTextPlaceholder').plusAsTab();
+
+  $("textarea").autosize();
+
+	// Lazy load von Bildern installieren
+	setLazy();
+	lazyLoad();
+	$(window).on('scroll', lazyLoad);
+	
+});
